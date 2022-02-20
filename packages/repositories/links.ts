d23e809b70a9565a24751423590416930247ab63
Roman Soirra <aaorris@gmail.com>
2022-02-19 23:32:27 -0500

    Refactor link repository. Allow deletion (locally)

--- packages/repositories/links.ts
+++ packages/repositories/links.ts
@@ -1,29 +1,19 @@
-import client from "./providers/dynamo";
+// import client from "./providers/dynamo";
+import { find, deleteSome } from "./details/dynamoKeyValue";
+
+// We create a secondary index "uid-index"
+// with only-keys
+export const deleteLink = async (table, uid) => {
+  const result = await deleteSome({ table, uid });
+  return result;
+};
 
 export const getLinksForTag = async (table, tag) => {
-  const result = await client.query({
-    KeyConditionExpression: "pk = :pk",
-    ExpressionAttributeValues: {
-      ":pk": tag,
-    },
-    TableName: table,
-    ScanIndexForward: false,
-  });
-  return {
-    items: result.Items,
-  };
+  const [items] = await find({ table, pk: tag });
+  return { items };
 };
 
 export const getTags = async (table) => {
-  const result = await client.query({
-    KeyConditionExpression: "pk = :pk",
-    ExpressionAttributeValues: {
-      ":pk": "//tags",
-    },
-    TableName: table,
-    ScanIndexForward: false,
-  });
-  return {
-    items: result.Items,
-  };
+  const [items] = await find({ table, pk: "//tags" });
+  return { items };
 };
